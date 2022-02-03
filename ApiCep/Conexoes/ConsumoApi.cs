75ea11ed83b6edb38a1030f99efa5338cdcce175
André Martins <andre.felipe47@hotmail.com>
2022-02-03 09:00:51 -0300

    Caminho feliz

---
+++ ApiCep/Conexoes/ConsumoApi.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace ApiCep.Conexoes
+{
+    public static class ConsumoApi
+    {
+        public static readonly HttpClient _client = new HttpClient();
+
+        public static T Get<T>(string url)
+        {
+            var response = _client.GetAsync(url).GetAwaiter().GetResult();
+
+            var content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return JsonConvert.DeserializeObject<T>(content);
+            else
+                throw new InvalidOperationException("Serviço de consulta ao cep indisponível no momento.");
+        }
+    }
+}
---
+++ ApiCep/Controllers/CepController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCep.Controllers
+{
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        [HttpGet("v1/ConsultarCep")]
+        public Contratos.ViaCep.Response ConsultarCep(string cep)
+        {
+            var retorno = Conexoes.ConsumoApi.Get<Contratos.ViaCep.Response>("https://viacep.com.br/ws/"+ cep + "/json/");
+            return retorno;
+        }
+    }
+}
